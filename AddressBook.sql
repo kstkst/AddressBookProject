e30dae61c0f9e8f6dfdca35ab769e0f157221bdc
yhan5522 <gks963@naver.com>
2019-10-31 00:09:33 +0900

    database query

---
+++ AddressBook.sql
@@ -0,0 +1,9 @@
+create table Address (
+	id int not null auto_increment primary key,
+    name varchar(50) not null,
+    relationship varchar(50) not null,
+    email varchar(50) not null,
+    phoneNumber varchar(50) not null
+);
+
+select * from Address;
